050e94be443ce517fec6be66c14e512655ee086e
= <98_han@naver.com>
2024-03-30 17:49:03 +0900

    feat: 변수와 가변성

---
+++ variables/src/main.rs
@@ -0,0 +1,25 @@
+
+fn main() {
+// # 가변(mut)
+//     let mut x = 5;
+//     println!("The Value of x is: {x}");
+//     x = 6;
+//     println!("The value of x is: {x}")
+
+// # 섀도잉(shadowing)
+
+    // let x = 5;
+    //
+    //     let x = x + 1;
+    //
+    //     {
+    //         let x = x * 2;
+    //         println!("The value of x in the inner scope is: {x}");
+    //     }
+    //
+    //     println!("The value of x is: {x}");
+
+    let spaces = "   ";   // let mut은 변수의 타입을 바꿀 수 없음
+    let spaces = spaces.len();
+}
+
